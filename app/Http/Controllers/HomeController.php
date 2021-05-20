09ede012e2b60c97053d35dc70c5b91688b93bda
Camilo <kamitriana7@gmail.com>
2021-05-20 13:36:26 -0500

    Adding content to home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace sisVentas\Http\Controllers;
 
 use sisVentas\Http\Requests;
 use Illuminate\Http\Request;
+use DB;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -22,8 +24,25 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('home');
+        if ($request){
+            $idusuario = (Auth::user()->id);
+            $asignatura=DB::table('asignatura as asi')
+            ->join('asignaturaUsuario as asiU','asi.id_asignatura','=','asiU.id_asignatura')
+            ->join('users as u','asiU.id_usuario','=','u.id')
+            ->select('asi.id_asignatura','asi.codigo','asi.nombre')
+            ->where('u.id','=',$idusuario)
+            ->orderBy('id_asignatura','asc')
+            ->paginate(10);
+
+            $usuario=DB::table('users as u')
+                ->join('usertype as ust','u.usertype_id_usertype','=','ust.id_usertype')
+                ->select('u.id','u.name','u.email','ust.name as usertype_id_usertype')
+                ->orderBy('id','asc')
+                ->paginate(10);
+            
+            return view('home',["asignatura"=>$asignatura,'usuario'=>$usuario]);
+        }
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,81 @@
 @extends('layouts.app')
 
 @section('content')
-<div id="fondopng">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard</div>
+    <div id="fondopng">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h1>Bienvenido {{ Auth::user()->name }}</h1>
+                        </div>
 
-                    <div class="panel-body">
-                        You are logged in!
-                    </div>
-                </div>
-                <br>
-                @if (session('info'))
-                                <div class="alert alert-success">
-                                    {{ session('info') }}
+                        <div class="panel-body">
+                            @if (Auth::user()->usertype_id_usertype > 1)
+                                @if (count($asignatura) > 0)
+                                    <div class="row">
+                                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                            <div class="table-responsive">
+                                                <table 
+                                                    class="table table-striped table-bordered table-condensed table-hover">
+                                                    <caption>
+                                                        <h3>ASIGNATURAS ASIGNADAS</h3>
+                                                    </caption>
+                                                    <thead>
+                                                        <th id="tablehome">ID ASIGNATURA</th>
+                                                        <th id="tablehome">CODIGO</th>
+                                                        <th id="tablehome">NOMBRE</th>
+                                                    </thead>
+                                                    @foreach ($asignatura as $asi)
+                                                        <tr>
+                                                            <td>{{ $asi->id_asignatura }}</td>
+                                                            <td>{{ $asi->codigo }}</td>
+                                                            <td>{{ $asi->nombre }}</td>
+                                                        </tr>
+                                                    @endforeach
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                @else
+                                    <h4 id="titleuser">No hay materias asignadas</h4>
+                                @endif
+                            @else
+                                <div class="row">
+                                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                        <div class="table-responsive">
+                                            <table class="table table-striped table-bordered table-condensed table-hover">
+                                                <caption>
+                                                    <h3>USUARIOS REGISTRADOS</h3>
+                                                </caption>
+                                                <thead>
+                                                    <th id="tablehome">ID</th>
+                                                    <th id="tablehome">NOMBRE</th>
+                                                    <th id="tablehome">CORREO</th>
+                                                    <th id="tablehome">TIPO DE USUARIO</th>
+                                                </thead>
+                                                @foreach ($usuario as $usu)
+                                                    <tr>
+                                                        <td>{{ $usu->id }}</td>
+                                                        <td>{{ $usu->name }}</td>
+                                                        <td>{{ $usu->email }}</td>
+                                                        <td>{{ $usu->usertype_id_usertype }}</td>
+                                                    </tr>
+                                                @endforeach
+                                            </table>
+                                        </div>
+                                    </div>
                                 </div>
                             @endif
+                        </div>
+                        @if (session('info'))
+                            <div id="mensajepermisos" class="alert alert-success">
+                                {{ session('info') }}
+                            </div>
+                        @endif
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
